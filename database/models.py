2c30c0d6a7c3872d83144ff236a65745b4e0787f
Jafari <amiralijafri5831@gmail.com>
2026-07-14 12:31:49 +0330

    completed in accordance with Milestone 1. class User added.

--- database/models.py
+++ database/models.py
@@ -1,15 +1,31 @@
 from dataclasses import dataclass
-from typing import  Optional
+from typing import Optional
+from datetime import datetime
+
 
 @dataclass
-class MachRecord:
-    id : Optional[int] = None
-    player_name : str = ""
-    score : int = 0
-    timestamp  : Optional[str] = None
-    
+class MachHistory:
+    id: Optional[int] = None
+    player_name: str = ""
+    score: int = 0
+    timestamp: Optional[str] = None
+
     def __post_init__(self):
-        if self.player_name == None or self.player_name.strip() == 0:
-            raise ValueError("نام نمی‌میتواند خالی باشد")
+        if self.player_name == None or len(self.player_name.strip()) == 0:
+            raise ValueError("Player name cannot be empty")
         if self.score < 0:
-            raise ValueError("امتیاز نمی‌تواند منفی باشد")
+            raise ValueError("score cannot be negative")
+        if self.timestamp is None:
+            self.timestamp = datetime.now().isoformat()
+
+
+@dataclass
+class User:
+    username = str
+    password_hash = str
+
+    def __post_init__(self):
+        if not self.username or len(self.username.strip()) < 3:
+            raise ValueError("The username must be at least 3 characters long.")
+        if not self.password_hash:
+            raise ValueError("password hash cannot be empty")
